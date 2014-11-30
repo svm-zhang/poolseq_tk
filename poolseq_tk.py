7d1d77bf6b54ee022b928520fc761e800740fcf6
Simo Zhang <zhangsimo850114@gmail.com>
2014-11-29 23:39:30 -0500

    further clear out some funcs, change the way show overview subcommand interface

--- poolseq_tk.py
+++ poolseq_tk.py
@@ -37,98 +37,96 @@ class SubcommandHelpFormatter(argparse.RawDescriptionHelpFormatter):
 		else:
 			return parts
 
-def parse_cmd():
-	parser = argparse.ArgumentParser(description="Association mapping using pooled samples")
-	sub_parsers = parser.add_subparsers(title="Commands", metavar="[command]", dest="command")
+def show_subcommand_overviews():
+	parser = argparse.ArgumentParser(description="Toolkits for Genome-wide Association Mapping using Pooled Sequencing")
+	sub_parsers = parser.add_subparsers(title="Commands", metavar="", dest="command")
 
 	# merging two mpileup files
 	collapse_parser = sub_parsers.add_parser("collapse", help="collapse two mpileupis that covering the same set of SNPs")
-	collapse_parser.add_argument("-m1", metavar="FILE", dest="m1", help="one of the two mpileup files")
-	collapse_parser.add_argument("-m2", metavar="FILE", dest="m2", help="one of the two mpileup files")
-	collapse_parser.add_argument("-offset1", metavar="INT", dest="offset1", type=int, default=0, help="offset add in for the first mpileup file specified by -m1")
-	collapse_parser.add_argument("-offset2", metavar="INT", dest="offset2", type=int, default=0, help="offset add in for the second mpileup file specified by -m2")
-	collapse_parser.add_argument("-snps", metavar="FILE", dest="snps", help="a list of SNP positions. e.g. chr	pos")
-	collapse_parser.add_argument("-p", metavar="PREFIX", dest="outp", help="prefix for output file")
-	collapse_parser.set_defaults(func=collapse_mpileups)
+	filter_parser = sub_parsers.add_parser("filter", help="filter out SNPs in allele counts file that do not satisfy given conditions")
+	count_parser = sub_parsers.add_parser("count", help="counting alleles given mpileup file")
+	mergeAC_parser = sub_parsers.add_parser("mergeAC", help="combining allele counts from replicates")
+	fisher_parser = sub_parsers.add_parser("fisher", help="run Fisher's Exact Test at each SNP")
+	cmh_parser = sub_parsers.add_parser("cmh", help="run Cochran-Mantel-Haenszel test with multi-testing adjustment")
+	plot_parser = sub_parsers.add_parser("plot", help="making Manhattan and Q-Q plots")
+	adjust_parser = sub_parsers.add_parser("adjust", help="getting significant SNPs with FDR correction")
+	overlap_parser = sub_parsers.add_parser("overlap", help="overlapion of significant SNPs identified from two pools")
+	diff_parser = sub_parsers.add_parser("diff", help="get SNPs that significant in one replicate but not in the other")
+
+
+#	collapse_parser.add_argument("-m1", metavar="FILE", dest="m1", help="one of the two mpileup files")
+#	collapse_parser.add_argument("-m2", metavar="FILE", dest="m2", help="one of the two mpileup files")
+#	collapse_parser.add_argument("-offset1", metavar="INT", dest="offset1", type=int, default=0, help="offset add in for the first mpileup file specified by -m1")
+#	collapse_parser.add_argument("-offset2", metavar="INT", dest="offset2", type=int, default=0, help="offset add in for the second mpileup file specified by -m2")
+#	collapse_parser.add_argument("-snps", metavar="FILE", dest="snps", help="a list of SNP positions. e.g. chr	pos")
+#	collapse_parser.add_argument("-p", metavar="PREFIX", dest="outp", help="prefix for output file")
+#	collapse_parser.set_defaults(func=collapse_mpileups)
 
 	# Arguments for filtering unwanted SNPs
-	filter_parser = sub_parsers.add_parser("filter", help="filter out SNPs in allele counts file that do not satisfy given conditions")
-	filter_parser.add_argument("-ac", metavar="FILE", dest="ac_file", help="allele counts file")
-	filter_parser.add_argument("-o", metavar="FILE", dest="out", help="output file without filtered SNPs")
-	filter_parser.add_argument("-min_ref_ac", metavar="INT", dest="min_ref_ac", type=int, default=5, help="minimum number of the ref allele (3rd column) per sample/pool")
-	filter_parser.add_argument("-min_alt_ac", metavar="INT", dest="min_alt_ac", type=int, default=5, help="minimum number of the alt allele (4th column) per sample/pool")
-	filter_parser.add_argument("-min_cov", metavar="INT", dest="min_cov", type=int, default=10, help="specify minimum coverage per site per sample/pool")
-	filter_parser.set_defaults(func=filter_SNPs)
+#	filter_parser.add_argument("-ac", metavar="FILE", dest="ac_file", help="allele counts file")
+#	filter_parser.add_argument("-o", metavar="FILE", dest="out", help="output file without filtered SNPs")
+#	filter_parser.add_argument("-min_ref_ac", metavar="INT", dest="min_ref_ac", type=int, default=5, help="minimum number of the ref allele (3rd column) per sample/pool")
+#	filter_parser.add_argument("-min_alt_ac", metavar="INT", dest="min_alt_ac", type=int, default=5, help="minimum number of the alt allele (4th column) per sample/pool")
+#	filter_parser.add_argument("-min_cov", metavar="INT", dest="min_cov", type=int, default=10, help="specify minimum coverage per site per sample/pool")
+#	filter_parser.set_defaults(func=filter_SNPs)
 
 	# counting alleles
-#	count_parser = sub_parsers.add_parser("count", help="counting alleles given mpileup file")
 #	count_parser.add_argument("-i", metavar="FILE", dest="out_count", help="output file of allele counts at each SNP")
 #	count_parser.add_argument("mpileups", metavar="MILEUP FILE", nargs='+', help="mpileup files")
 #	count_parser.set_defaults(func=run_count)
 
 	# arguments for combining allele counts from replicates
-	combineAC_parser = sub_parsers.add_parser("combineAC", help="combining allele counts from replicates")
-	combineAC_parser.add_argument("-o", metavar="FILE", dest="out", help="output file of combined counts at each SNP across replicates")
-	combineAC_parser.add_argument("acs", metavar="ac_file", nargs='+', help="allele counts files")
-	combineAC_parser.set_defaults(func=combineAC)
+#	mergeAC_parser.add_argument("-o", metavar="FILE", dest="out", help="output file of combined counts at each SNP across replicates")
+#	mergeAC_parser.add_argument("acs", metavar="ac_file", nargs='+', help="allele counts files")
+#	mergeAC_parser.set_defaults(func=mergeAC)
 
 	# arguments for running Fisher's Exact test
-	fisher_parser = sub_parsers.add_parser("fisher", help="run Fisher's Exact Test at each SNP")
-	fisher_parser.add_argument("-ac", metavar="FILE", dest="ac_file", help="allele counts for one pool")
-	fisher_parser.add_argument("-outp", metavar="PREFIX", dest="outp", help="output file for Fisher's Exact tests")
-	fisher_parser.add_argument("-t", metavar="INT", dest="nproc", type=int, default=1, help="Specify number of processes running simultaneously")
-	fisher_parser.add_argument("-adj_cutoff", metavar="FLOAT", dest="adj_cutoff", type=float, default=0.05, help="specify the cutoff below which adjusted p-values will be considered as significant")
-	fisher_parser.add_argument("-adj_method", metavar="STR", dest="adj_method", default="BH", choices=["holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none"], help="specify the adjustment methods")
-	fisher_parser.add_argument("-direction", metavar="STR", dest="oddsr_direction", choices=["greater", "less"], help="specify whether odds ration greater, or less, than 1")
+#	fisher_parser.add_argument("-ac", metavar="FILE", dest="ac_file", help="allele counts for one pool")
+#	fisher_parser.add_argument("-outp", metavar="PREFIX", dest="outp", help="output file for Fisher's Exact tests")
+#	fisher_parser.add_argument("-t", metavar="INT", dest="nproc", type=int, default=1, help="Specify number of processes running simultaneously")
+#	fisher_parser.add_argument("-adj_cutoff", metavar="FLOAT", dest="adj_cutoff", type=float, default=0.05, help="specify the cutoff below which adjusted p-values will be considered as significant")
+#	fisher_parser.add_argument("-adj_method", metavar="STR", dest="adj_method", default="BH", choices=["holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none"], help="specify the adjustment methods")
+#	fisher_parser.add_argument("-direction", metavar="STR", dest="oddsr_direction", choices=["greater", "less"], help="specify whether odds ration greater, or less, than 1")
 #	fisher_parser.set_defaults(func=run_fisher)
 
 	# arguments for running Cochran-Mantel-Haenszel test
-	cmh_parser = sub_parsers.add_parser("cmh", help="run Cochran-Mantel-Haenszel test with multi-testing adjustment")
-	cmh_parser.add_argument("-table", metavar="FILE", dest="table_file", required=True, help="output file with the table that CMH test run on")
-	cmh_parser.add_argument("-outp", metavar="PREFIX", dest="outp", required=True, help="output file with CMH test results")
-	cmh_parser.add_argument("-t", metavar="INT", dest="nproc", type=int, default=1, help="Specify number of processes running simultaneously")
-	cmh_parser.add_argument("-adj_cutoff", metavar="FLOAT", dest="adj_cutoff", type=float, default=0.05, help="specify the cutoff below which adjusted p-values will be considered as significant")
-	cmh_parser.add_argument("-adj_method", metavar="STR", dest="adj_method", default="BH", choices=["holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none"], help="specify the adjustment methods")
-	cmh_parser.add_argument("-direction", metavar="STR", dest="oddsr_direction", choices=["greater", "less"], help="specify whether odds ration greater, or less, than 1")
+#	cmh_parser.add_argument("-table", metavar="FILE", dest="table_file", required=True, help="output file with the table that CMH test run on")
+#	cmh_parser.add_argument("-outp", metavar="PREFIX", dest="outp", required=True, help="output file with CMH test results")
+#	cmh_parser.add_argument("-t", metavar="INT", dest="nproc", type=int, default=1, help="Specify number of processes running simultaneously")
+#	cmh_parser.add_argument("-adj_cutoff", metavar="FLOAT", dest="adj_cutoff", type=float, default=0.05, help="specify the cutoff below which adjusted p-values will be considered as significant")
+#	cmh_parser.add_argument("-adj_method", metavar="STR", dest="adj_method", default="BH", choices=["holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none"], help="specify the adjustment methods")
+#	cmh_parser.add_argument("-direction", metavar="STR", dest="oddsr_direction", choices=["greater", "less"], help="specify whether odds ration greater, or less, than 1")
 #	cmh_parser.set_defaults(func=run_cmh)
 
 	# arguments for making Q-Q plot and Manhattan plot
-	plot_parser = sub_parsers.add_parser("plot", help="making Manhattan and Q-Q plots")
-	plot_parser.add_argument("-i", metavar="FILE", dest="input", required=True, help="input file of test results with all SNPs, e.g. *.fisher.all, *.cmh.all")
-	plot_parser.add_argument("-interests", metavar="FILE", dest="interests_snps", help="file of a list of SNPs of interests. These SNPs will be highlighted in the Manhattan plot")
-	plot_parser.add_argument("-outp", metavar="PREFIX", dest="outp", help="prefix of output file")
-	plot_parser.add_argument("-adj_cutoff", metavar="FLOAT", dest="adj_cutoff", type=float, default=0.05, help="specify the cutoff below which adjusted p-values will be considered as significant")
-	plot_mutual_group = plot_parser.add_mutually_exclusive_group(required=True)
-	plot_mutual_group.add_argument("-pdf", dest="pdf", action="store_true", help="output qqplot in pdf format")
-	plot_mutual_group.add_argument("-png", dest="png", action="store_true", help="output qqplot in pdf format")
+#	plot_parser.add_argument("-i", metavar="FILE", dest="input", required=True, help="input file of test results with all SNPs, e.g. *.fisher.all, *.cmh.all")
+#	plot_parser.add_argument("-interests", metavar="FILE", dest="interests_snps", help="file of a list of SNPs of interests. These SNPs will be highlighted in the Manhattan plot")
+#	plot_parser.add_argument("-outp", metavar="PREFIX", dest="outp", help="prefix of output file")
+#	plot_parser.add_argument("-adj_cutoff", metavar="FLOAT", dest="adj_cutoff", type=float, default=0.05, help="specify the cutoff below which adjusted p-values will be considered as significant")
+#	plot_mutual_group = plot_parser.add_mutually_exclusive_group(required=True)
+#	plot_mutual_group.add_argument("-pdf", dest="pdf", action="store_true", help="output qqplot in pdf format")
+#	plot_mutual_group.add_argument("-png", dest="png", action="store_true", help="output qqplot in pdf format")
 #	plot_parser.set_defaults(func=making_plot)
 
 	# arguments for adjusting p-values
-	adjust_parser = sub_parsers.add_parser("adjust", help="getting significant SNPs with FDR correction")
-	adjust_parser.add_argument("-fisher", metavar="FILE", dest="power_file", help="*.fisher file with a set of SNPs at which statistical test ran with power")
-	adjust_parser.add_argument("-outp", metavar="PREFIX", dest="outp", help="prefix of output file")
-	adjust_parser.add_argument("-cutoff", metavar="FLOAT", dest="adj_cutoff", type=float, default=0.05, help="specify the FDR rate cutoff")
+#	adjust_parser.add_argument("-fisher", metavar="FILE", dest="power_file", help="*.fisher file with a set of SNPs at which statistical test ran with power")
+#	adjust_parser.add_argument("-outp", metavar="PREFIX", dest="outp", help="prefix of output file")
+#	adjust_parser.add_argument("-cutoff", metavar="FLOAT", dest="adj_cutoff", type=float, default=0.05, help="specify the FDR rate cutoff")
 #	adjust_parser.set_defaults(func=multi_testing_correction)
 
-	# arguments for get intersection of significant SNPs between pools, or across replicates
-	intersect_parser = sub_parsers.add_parser("intersect", help="intersection of significant SNPs identified from two pools")
-	intersect_parser.add_argument("-a", metavar="FILE", dest="file_a", help="significant SNPs identified from pool A")
-	intersect_parser.add_argument("-b", metavar="FILE", dest="file_b", help="significant SNPs identified from pool B")
-	intersect_parser.add_argument("-o", metavar="FILE", dest="out", help="output file of intersection of significant SNPs identified from both pools")
-	intersect_parser.set_defaults(func=intersect)
+	# arguments for get overlapion of significant SNPs between pools, or across replicates
+#	overlap_parser.add_argument("-a", metavar="FILE", dest="file_a", help="significant SNPs identified from pool A")
+#	overlap_parser.add_argument("-b", metavar="FILE", dest="file_b", help="significant SNPs identified from pool B")
+#	overlap_parser.add_argument("-o", metavar="FILE", dest="out", help="output file of overlapion of significant SNPs identified from both pools")
+#	overlap_parser.set_defaults(func=overlap)
 
 	# arugments for get significant SNPs in one pool/replicate, but not in the other
-	diff_parser = sub_parsers.add_parser("diff", help="get SNPs that significant in one replicate but not in the other")
-	diff_parser.add_argument("-sigrep1", metavar="FILE", dest="sigrep1_file", help="file with a set of significant SNPs identified from one replicated, e.g. *.b.fdr, *.b.fdr.expect")
-	diff_parser.add_argument("-rep2", metavar="FILE", dest="rep2_file", help="file with all SNPs identified from another replicate, e.g. *.a.power")
-	diff_parser.add_argument("-o", metavar="FILE", dest="out", help="output file with SNPs significant in -sigrep1 file but not in -rep2 file")
+#	diff_parser.add_argument("-sigrep1", metavar="FILE", dest="sigrep1_file", help="file with a set of significant SNPs identified from one replicated, e.g. *.b.fdr, *.b.fdr.expect")
+#	diff_parser.add_argument("-rep2", metavar="FILE", dest="rep2_file", help="file with all SNPs identified from another replicate, e.g. *.a.power")
+#	diff_parser.add_argument("-o", metavar="FILE", dest="out", help="output file with SNPs significant in -sigrep1 file but not in -rep2 file")
 #	diff_parser.set_defaults(func=call_diff)
 
 	return parser.parse_args()
-
-def run_count():
-	from count import count_alleles
-	count_alleles()
 
 def get_SNPs(snps_file):
 	snp_pos = collections.defaultdict(tuple)
@@ -235,7 +233,7 @@ def filter_SNPs(args):
 					counts[2]+counts[3] >= args.min_cov):
 					fOUT.write(line)
 
-def combineAC(args):
+def mergeAC(args):
 	''' combine allele counts across replicates '''
 	allele_counts = collections.defaultdict(list)
 	data = collections.defaultdict(list)
@@ -262,52 +260,19 @@ def cat_split_files(file_list, out_file):
 		for file in sorted(file_list):
 			shutil.copyfileobj(open(file, 'r'), fOUT)
 
-def assign_tables(tables, task_q, nproc):
-	''' assigning each process a number of tables '''
-	ntables = len(tables)
-	ntables_per_proc = ntables/nproc + 1
-	i = 0
-	nth_job = 1
-	while i + ntables_per_proc <= ntables:
-		task_q.put((dict(sorted(tables.items())[i:i+ntables_per_proc+1]), nth_job))
-		i += ntables_per_proc + 1
-		nth_job += 1
-	task_q.put((dict(sorted(tables.items())[i:]), nth_job))
-
-def results_outputter(fOUT, pos, chr, bases, tables, pval, corr_pval, odds_ratio):
-	fOUT.write("%s\t%d\t%s" %(chr, pos, bases))
-	i = 0
-	while i <= len(tables) - 4:
-		fOUT.write("\t%s" %(":".join(tables[i:i+4])))
-		i += 4
-	fOUT.write("\t%.8f\t%.8f\t%.8f\n" %(pval, corr_pval, odds_ratio))
-	fOUT.flush()
-
-def intersect(args):
-	''' getting SNPs identified from both pools '''
-	snp_a = collections.defaultdict(list)
-	with open(args.file_a, 'r') as fA:
-		for line in fA:
-			tmp_line = line.strip().split("\t")
-			snp_a[int(tmp_line[1])] = tmp_line
-	sys.stdout.write("[pool_gwas]: %d SNPs parsed from %s\n" %(len(snp_a), os.path.basename(args.file_a)))
-
-	num_intersection = 0
-	with open(args.out, 'w') as fOUT:
-		with open(args.file_b, 'r') as fB:
-			for line in fB:
-				tmp_line = line.strip().split("\t")
-				if int(tmp_line[1]) in snp_a:
-					num_intersection += 1
-					fOUT.write("%s\t%s\n" %("\t".join(snp_a[int(tmp_line[1])]), "\t".join(tmp_line[-4:])))
-	sys.stdout.write("[pool_gwas]: %d SNPs identified from both pools\n" %(num_intersection))
-
 def main():
 	subcommand = sys.argv[1]
 	if subcommand == "count":
-		run_count()
-#	args = parse_cmd()
-#	args.func(args)
+		import sz_acount
+		sz_acount.count_alleles()
+	elif subcommand == "fisher":
+		import sz_fisher
+		sz_fisher.run_fisher()
+	elif subcommand == "cmh":
+		import sz_cmh
+		sz_cmh.run_cmh()
+	elif subcommand in ["-h", "--help"]:
+		show_subcommand_overviews()
 
 if __name__ == "__main__":
 	main()
