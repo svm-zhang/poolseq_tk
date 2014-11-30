b9f722b9823321012bede66bf9b90ba33247cfed
Simo Zhang <zhangsimo850114@gmail.com>
2014-11-30 17:17:50 -0500

    further clear out, almost there

--- poolseq_tk.py
+++ poolseq_tk.py
@@ -53,28 +53,15 @@ def show_subcommand_overviews():
 	overlap_parser = sub_parsers.add_parser("overlap", help="overlapion of significant SNPs identified from two pools")
 	diff_parser = sub_parsers.add_parser("diff", help="get SNPs that significant in one replicate but not in the other")
 
-#	collapse_parser.add_argument("-m1", metavar="FILE", dest="m1", help="one of the two mpileup files")
-#	collapse_parser.add_argument("-m2", metavar="FILE", dest="m2", help="one of the two mpileup files")
-#	collapse_parser.add_argument("-offset1", metavar="INT", dest="offset1", type=int, default=0, help="offset add in for the first mpileup file specified by -m1")
-#	collapse_parser.add_argument("-offset2", metavar="INT", dest="offset2", type=int, default=0, help="offset add in for the second mpileup file specified by -m2")
-#	collapse_parser.add_argument("-snps", metavar="FILE", dest="snps", help="a list of SNP positions. e.g. chr	pos")
-#	collapse_parser.add_argument("-p", metavar="PREFIX", dest="outp", help="prefix for output file")
 #	collapse_parser.set_defaults(func=collapse_mpileups)
 
 	# Arguments for filtering unwanted SNPs
-#	filter_parser.add_argument("-ac", metavar="FILE", dest="ac_file", help="allele counts file")
-#	filter_parser.add_argument("-o", metavar="FILE", dest="out", help="output file without filtered SNPs")
-#	filter_parser.add_argument("-min_ref_ac", metavar="INT", dest="min_ref_ac", type=int, default=5, help="minimum number of the ref allele (3rd column) per sample/pool")
-#	filter_parser.add_argument("-min_alt_ac", metavar="INT", dest="min_alt_ac", type=int, default=5, help="minimum number of the alt allele (4th column) per sample/pool")
-#	filter_parser.add_argument("-min_cov", metavar="INT", dest="min_cov", type=int, default=10, help="specify minimum coverage per site per sample/pool")
 #	filter_parser.set_defaults(func=filter_SNPs)
 
 	# counting alleles
 #	count_parser.set_defaults(func=run_count)
 
 	# arguments for combining allele counts from replicates
-#	mergeAC_parser.add_argument("-o", metavar="FILE", dest="out", help="output file of combined counts at each SNP across replicates")
-#	mergeAC_parser.add_argument("acs", metavar="ac_file", nargs='+', help="allele counts files")
 #	mergeAC_parser.set_defaults(func=mergeAC)
 
 #	fisher_parser.set_defaults(func=run_fisher)
@@ -90,131 +77,7 @@ def show_subcommand_overviews():
 
 	return parser.parse_args()
 
-def get_SNPs(snps_file):
-	snp_pos = collections.defaultdict(tuple)
-	with open(snps_file, 'r') as fSNP:
-		for line in fSNP:
-			if not line.startswith('#'):
-				tmp_line = line.strip().split("\t")
-				snp_pos[int(tmp_line[0])] = (tmp_line[1], tmp_line[2])
-	return snp_pos
 
-def read_mpileup(mpileup_file, offset):
-	sys.stdout.write("[pool_gwas]: reading %s ..." %(mpileup_file))
-	mpileup_info = collections.defaultdict(tuple)
-	with open(mpileup_file, 'r') as fMPILEUP:
-		for line in fMPILEUP:
-			tmp_line = line.strip().split("\t")
-			if int(tmp_line[3]) == 0:
-				mpileup_info[int(tmp_line[1])+offset] = (tmp_line[2].upper(), "N/A")
-			else:
-				mpileup_info[int(tmp_line[1])+offset] = (tmp_line[2].upper(), tmp_line[4])
-	sys.stdout.write(" [done]\n")
-	return mpileup_info
-
-def collapse_cover_bases(ref_base, alt_base, cover_bases, collapsed_cover_base_str):
-	if cover_bases != "N/A":
-		i = 0
-		while i <= len(cover_bases)-1:
-			if cover_bases[i] == '.':
-				collapsed_cover_base_str += ref_base
-				i += 1
-			elif cover_bases[i] == ',':
-				collapsed_cover_base_str += ref_base.lower()
-				i += 1
-			elif cover_bases[i] == alt_base:
-				collapsed_cover_base_str += alt_base
-				i += 1
-			elif cover_bases[i] ==  alt_base.lower():
-				collapsed_cover_base_str += alt_base.lower()
-				i += 1
-			elif cover_bases[i] in ['+', '-']:
-				len_indel = int(cover_bases[i+1])
-				i += len_indel + 1 + 1
-			elif cover_bases[i] == '^':
-				i += 2
-			else:
-				i += 1
-	return collapsed_cover_base_str
-
-def collapse_mpileups(args):
-	snp_pos = get_SNPs(args.snps)
-
-	m1_info = read_mpileup(args.m1, args.offset1)
-	m2_info = read_mpileup(args.m2, args.offset2)
-
-	sys.stdout.write("[pool_gwas] %s: %d SNPs parsed\n" %(os.path.basename(args.m1), len(m1_info)))
-	sys.stdout.write("[pool_gwas] %s: %d SNPs parsed\n" %(os.path.basename(args.m2), len(m2_info)))
-
-	sys.stdout.write("[pool_gwas]: collapsing mpileups %s and %s ..." %(os.path.basename(args.m1), os.path.basename(args.m2)))
-	merged_mpileup = args.outp + ".collapse.snps.mpileup"
-	with open(merged_mpileup, 'w') as fCOLLAPSE:
-		for pos in sorted(snp_pos.iterkeys()):
-			cover_base_str = ""
-			if pos in m1_info and pos in m2_info:
-				if snp_pos[pos][0] == m1_info[pos][0] and snp_pos[pos][1] == m2_info[pos][0]:
-					cover_base_str = collapse_cover_bases(m1_info[pos][0], m2_info[pos][0], m1_info[pos][1], cover_base_str)
-					cover_base_str = collapse_cover_bases(m2_info[pos][0], m1_info[pos][0], m2_info[pos][1], cover_base_str)
-					if cover_base_str == "":
-						fCOLLAPSE.write("2L\t%d\t%s\t%s\tN/A\n" %(pos, snp_pos[pos][0], snp_pos[pos][1]))
-					else:
-						fCOLLAPSE.write("2L\t%d\t%s\t%s\t%s\n" %(pos, snp_pos[pos][0], snp_pos[pos][1], cover_base_str))
-				else:
-					# this should bark if the same sites having different states
-					sys.stderr.write("SNP position: %d %s %s\t\tMpileup position: %d %s %s\n" %(pos, snp_pos[pos][0], snp_pos[pos][1], pos, m1_info[pos][0], m2_info[pos][0]))
-			elif pos not in m1_info and pos not in m2_info:
-					fCOLLAPSE.write("2L\t%d\t%s\t%s\t%s\n" %(pos, snp_pos[pos][0], snp_pos[pos][1], "N/A"))
-			elif pos in m1_info and pos not in m2_info:
-					cover_base_str = collapse_cover_bases(m1_info[pos][0], snp_pos[pos][1], m1_info[pos][1], cover_base_str)
-					if cover_base_str == "":
-						fCOLLAPSE.write("2L\t%d\t%s\t%s\tN/A\n" %(pos, snp_pos[pos][0], snp_pos[pos][1]))
-					else:
-						fCOLLAPSE.write("2L\t%d\t%s\t%s\t%s\n" %(pos, snp_pos[pos][0], snp_pos[pos][1], cover_base_str))
-			elif pos not in m1_info and pos in m2_info:
-					cover_base_str = collapse_cover_bases(m2_info[pos][0], snp_pos[pos][0], m2_info[pos][1], cover_base_str)
-					if cover_base_str == "":
-						fCOLLAPSE.write("2L\t%d\t%s\t%s\tN/A\n" %(pos, snp_pos[pos][0], snp_pos[pos][1]))
-					else:
-						fCOLLAPSE.write("2L\t%d\t%s\t%s\t%s\n" %(pos, snp_pos[pos][0], snp_pos[pos][1], cover_base_str))
-
-	sys.stdout.write(" [done]\n")
-
-def filter_SNPs(args):
-	with open(args.out, 'w') as fOUT:
-		with open(args.ac_file, 'r') as fAC:
-			for line in fAC:
-				tmp_line = line.strip().split("\t")
-				ref_base = tmp_line[2]
-				alt_base  = tmp_line[3]
-				counts = map(int, tmp_line[4].split(':'))
-				if (counts[1] >= args.min_alt_ac and
-					counts[0] >= args.min_ref_ac and
-					counts[2] >= args.min_ref_ac and
-					counts[3] >= args.min_alt_ac and
-					counts[0]+counts[1] >= args.min_cov and
-					counts[2]+counts[3] >= args.min_cov):
-					fOUT.write(line)
-
-def mergeAC(args):
-	''' combine allele counts across replicates '''
-	allele_counts = collections.defaultdict(list)
-	data = collections.defaultdict(list)
-	for ac_file in args.acs:
-		with open(ac_file) as fAC:
-			for line in fAC:
-				tmp_line = line.strip().split()
-				pos = int(tmp_line[1])
-				if not pos in data:
-					data[pos] = tmp_line[0:4]
-				if not pos in allele_counts:
-					allele_counts[pos] = map(int, tmp_line[4].split(':'))
-				else:
-					allele_counts[pos] = map(sum, zip(allele_counts[pos], map(int, tmp_line[4].split(':'))))
-
-	# output to file
-	with open(args.out, 'w') as fOUT:
-		for pos in sorted(allele_counts.iterkeys()):
-			fOUT.write("%s\t%s\n" %("\t".join(data[pos]), ":".join(map(str, allele_counts[pos]))))
 
 def cat_split_files(file_list, out_file):
 	''' concatenate split files '''
@@ -224,7 +87,16 @@ def cat_split_files(file_list, out_file):
 
 def main():
 	subcommand = sys.argv[1]
-	if subcommand == "count":
+	if subcommand == "collapse":
+		import sz_collapse
+		sz_collapse.run_collapse()
+	elif subcommand == "filter":
+		import sz_filter
+		sz_filter.run_filter()
+	elif subcommand == "mergeAC":
+		import sz_mergeAC
+		sz_mergeAC.run_merge()
+	elif subcommand == "count":
 		import sz_acount
 		sz_acount.count_alleles()
 	elif subcommand == "fisher":
@@ -245,7 +117,8 @@ def main():
 	elif subcommand == "plot":
 		import sz_plotting
 		sz_plotting.making_plot()
-	elif subcommand in ["-h", "--help"]:
+#	elif subcommand in ["-h", "--help"]:
+	else:
 		show_subcommand_overviews()
 
 if __name__ == "__main__":
