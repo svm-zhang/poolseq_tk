a074a8f9c5692e3d5e5b7eecfc11e511b9994f29
Simo Zhang <zhangsimo850114@gmail.com>
2014-12-01 16:51:38 -0500

    move arguments definition back to poolseq_tk.py; add output STDOUT; add colortext; add check file existence

--- sz_acount.py
+++ sz_acount.py
@@ -3,20 +3,17 @@ import collections
 import sys
 import os
 
-def parse_cmd():
-	count_parser = argparse.ArgumentParser(description="counting alleles")
-	count_parser.add_argument("-o", metavar="FILE", dest="out_count", help="output file of allele counts at each SNP")
-	count_parser.add_argument("mpileups", metavar="MILEUP FILE", nargs='+', help="mpileup files")
-	return count_parser.parse_args()
+import sz_utils
+from colortext import ColorText
 
-def count_alleles():
-	args = parse_cmd()
-
+def run_count(args):
+	''' Counting alleles at each SNP in the given pileup files '''
 	ac = collections.defaultdict(tuple)
-	for mpileup in args.mpileups:
+	for pileup in args.pileups:
+		sz_utils.check_if_files_exist(pileup)
 		nsnps = 0
-		sys.stdout.write("[pool_gwas] counting alleles in %s:" %(os.path.basename(mpileup)))
-		with open(mpileup, 'r') as fMPILEUP:
+		ColorText().info("[poolseq_tk] counting alleles in %s:" %(os.path.basename(pileup)), "stderr")
+		with open(pileup, 'r') as fMPILEUP:
 			for line in fMPILEUP:
 				nsnps += 1
 				tmp_line = line.strip().split("\t")
@@ -25,21 +22,30 @@ def count_alleles():
 				alt_base = tmp_line[3]
 				ref_count, alt_count = 0, 0
 				if tmp_line[-1] != "N/A":
-					ref_count = tmp_line[-1].count(ref_base) + tmp_line[-1].count(ref_base.lower())
-					alt_count = tmp_line[-1].count(alt_base) + tmp_line[-1].count(alt_base.lower())
+					ref_count = tmp_line[-1].count(ref_base) + \
+								tmp_line[-1].count(ref_base.lower())
+					alt_count = tmp_line[-1].count(alt_base) + \
+								tmp_line[-1].count(alt_base.lower())
 				if pos not in ac:
 					ac[pos] = [tmp_line[0], ref_base, alt_base, str(ref_count), str(alt_count)]
 				else:
 					ac[pos] += [str(ref_count), str(alt_count)]
-		sys.stdout.write(" %d SNPs parsed\n" %(nsnps))
+		ColorText().info(" %d SNPs parsed\n" %(nsnps), "stderr")
 
-	sys.stdout.write("[pool_gwas] outputting allele counts to table ...")
-	with open(args.out_count, 'w') as fCOUNT:
-		for pos in sorted(ac.iterkeys()):
-			fCOUNT.write("%s\t%d\t%s" %(ac[pos][0], pos, "\t".join(ac[pos][1:3])))
-			i = 3
-			while i <= len(ac[pos])-4:
-				fCOUNT.write("\t%s" %(":".join(ac[pos][i:i+4])))
-				i += 4
-			fCOUNT.write("\n")
-	sys.stdout.write(" [done]\n")
+	fOUT = None
+	if args.out == sys.stdout:
+		fOUT = sys.stdout
+	else:
+		outdir = os.path.dirname(os.path.realpath(args.out))
+		sz_utils.make_dirs_if_necessary(outdir)
+		fOUT = open(args.out, 'w')
+	ColorText().info("[poolseq_tk] outputting allele counts to table ...", "stderr")
+	for pos in sorted(ac.iterkeys()):
+		fOUT.write("%s\t%d\t%s" %(ac[pos][0], pos, "\t".join(ac[pos][1:3])))
+		i = 3
+		while i <= len(ac[pos])-4:
+			fOUT.write("\t%s" %(":".join(ac[pos][i:i+4])))
+			i += 4
+		fOUT.write("\n")
+	ColorText().info(" [done]\n", "stderr")
+	fOUT.close()
