8e60115627f4526954d114a6ba881489bb3eff87
Simo Zhang <zhangsimo850114@gmail.com>
2014-12-01 18:15:21 -0500

    move arguments definition back to poolseq_tk.py; add checking file existence; add colortext; add STDOUT support

--- sz_mergeAC.py
+++ sz_mergeAC.py
@@ -3,19 +3,17 @@ import sys
 import collections
 import argparse
 
-def getopts():
-	usage = "Merging allele counts from multiple replicates"
-	mergeAC_parser = argparse.ArgumentParser(description=usage)
-	mergeAC_parser.add_argument("-o", metavar="FILE", dest="out", help="output file of combined counts at each SNP across replicates")
-	mergeAC_parser.add_argument("acs", metavar="ac_file", nargs='+', help="allele counts files")
-	return mergeAC_parser.parse_args()
+import sz_utils
+from colortext import ColorText
 
-def run_merge():
+def run_merge(args):
 	''' combine allele counts across replicates '''
-	args = getopts()
 	allele_counts = collections.defaultdict(list)
 	data = collections.defaultdict(list)
 	for ac_file in args.acs:
+		sz_utils.check_if_files_exist(ac_file)
+		ColorText().info("[poolseq_tk] reading and updating allele counts from %s ..."
+						 %(ac_file), "stderr")
 		with open(ac_file) as fAC:
 			for line in fAC:
 				tmp_line = line.strip().split()
@@ -26,10 +24,18 @@ def run_merge():
 					allele_counts[pos] = map(int, tmp_line[4].split(':'))
 				else:
 					allele_counts[pos] = map(sum, zip(allele_counts[pos], map(int, tmp_line[4].split(':'))))
+		ColorText().info(" [done]\n", "stderr")
 
 	# output to file
-	with open(args.out, 'w') as fOUT:
-		for pos in sorted(allele_counts.iterkeys()):
-			fOUT.write("%s\t%s\n" %("\t".join(data[pos]), ":".join(map(str, allele_counts[pos]))))
-
-
+	fOUT = None
+	if args.out == sys.stdout:
+		fOUT = sys.stdout
+	else:
+		outdir = os.path.dirname(os.path.realpath(args.out))
+		sz_utils.make_dirs_if_necessary(outdir)
+		fOUT = open(args.out, 'w')
+	ColorText().info("[poolseq_tk] outputting to %s ..."
+					 %(fOUT.name), "stderr")
+	for pos in sorted(allele_counts.iterkeys()):
+		fOUT.write("%s\t%s\n" %("\t".join(data[pos]), ":".join(map(str, allele_counts[pos]))))
+	ColorText().info(" [done]\n", "stderr")
