45dbd34f638f1eca0c63102d2652dff21fd4a788
Simo Zhang <zhangsimo850114@gmail.com>
2014-12-08 11:37:45 -0500

    now support filter .ac file generated from one pileup file; add comments

--- sz_filter.py
+++ sz_filter.py
@@ -1,3 +1,19 @@
+'''
+	poolseq_tk.py filter
+	Description: filter SNPs that do not satisfy the user-specified conditions
+	Author: Simo Zhang
+
+	Input: allele counts file generated from step 3
+
+	Output: filtered set of SNPs with their allele counts
+			(1) chr
+			(2) pos
+			(3) ref base
+			(4) alt base
+			(5) allele counts
+
+'''
+
 import os
 import sys
 import argparse
@@ -13,7 +29,7 @@ def run_filter(args):
 	else:
 		sz_utils.make_dirs_if_necessary(args.out)
 		fOUT = open(args.out, 'w')
-	before, after = 0, 0
+	before, after = 0, 0			# number of SNPs before and after filteration
 	with open(args.ac_file, 'r') as fAC:
 		for line in fAC:
 			tmp_line = line.strip().split("\t")
@@ -21,12 +37,31 @@ def run_filter(args):
 			ref_base = tmp_line[2]
 			alt_base  = tmp_line[3]
 			counts = map(int, tmp_line[4].split(':'))
-			if (counts[1] >= args.min_alt_ac and
-				counts[0] >= args.min_ref_ac and
-				counts[2] >= args.min_ref_ac and
-				counts[3] >= args.min_alt_ac and
-				counts[0]+counts[1] >= args.min_cov and
-				counts[2]+counts[3] >= args.min_cov):
+			if len(counts) < 2:
+				ColorText().error("At least two counts (separated by colon) required"
+								  "for column five\n")
+				sys.exit(1)
+			fail = 0
+			for i in range(len(counts)):
+				if i == 0:
+					if counts[i] < args.min_ref_ac:
+						fail = 1
+						break
+				elif i == 1:
+					if (counts[i] < args.min_alt_ac or
+					    counts[i] + counts[i-1] < args.min_cov):
+						fail = 1
+						break
+				elif i == 2:
+					if counts[i] < args.min_ref_ac:
+						fail = 1
+						break
+				elif i == 3:
+					if (counts[i] < args.min_alt_ac or
+					   counts[i] + counts[i-1] < args.min_cov):
+						fail = 1
+						break
+			if fail == 0:
 				fOUT.write(line)
 				after += 1
 	fOUT.close()
