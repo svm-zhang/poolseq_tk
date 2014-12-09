262d24c014d25255d0f0c5fa8aeb6ec957871453
Simo Zhang <zhangsimo850114@gmail.com>
2014-12-09 10:55:19 -0500

    add comments; stop outputting to *.expect for now;

--- sz_cmh.py
+++ sz_cmh.py
@@ -1,7 +1,28 @@
+'''
+	python poolseq_tk.py cmh
+	Description: Running Cochran-Mantel-Hasenzle test at each SNPs
+	Author: Simo V. Zhang
+
+	Input: allele counts file
+
+	Output: outprefix.all
+			(1) chr
+			(2) pos
+			(3) ref base
+			(4) alt base
+			(5) allele counts (more than two columns)
+			(6) raw p-values
+			(7) corrected p-values
+			(8) odds ratio
+
+			outprefix.fdr5: a subset of above SNPs survived FDR
+'''
+
 import os
 import sys
 import multiprocessing as mp
 import argparse
+import collections
 
 import sz_utils
 from colortext import ColorText
@@ -10,7 +31,7 @@ import rpy2.robjects as robjects
 import rpy2.robjects.packages as rpackages
 import rpy2.rlike.container as rlc
 
-def run_cmh():
+def run_cmh(args):
 	''' run Cochran-Mantel-Hasenzle test '''
 
 	sz_utils.make_dirs_if_necessary(os.path.dirname(args.outp))
@@ -19,7 +40,7 @@ def run_cmh():
 	tables = collections.defaultdict(list)
 	ntests = 0
 	tables, ntables_per_snp = sz_utils._count2table(args.table_file)
-	ColorText().info("[poolseq_tk] %d tables prepared\n" %(len(tables), "stderr"))
+	ColorText().info("[poolseq_tk] %d tables prepared\n" %(len(tables)), "stderr")
 
 	task_q = mp.JoinableQueue()
 	result_q = mp.Queue()
@@ -40,17 +61,22 @@ def run_cmh():
 			pvals.update(pvals_split)
 			odds_ratios.update(odds_ratios_split)
 			args.nproc -= 1
+		ColorText().info("[poolseq_tk]: Running CMH tests successfully\n", "stderr")
 
 		# correcting raw p-values
+		ColorText().info("[poolseq_tk]: multi-testing correction using %s method at %d%% level ..."
+						 %(args.adj_method, args.adj_cutoff*100), "stderr")
 		raw_pvals = [pvals[k] for k in sorted(pvals.iterkeys())]
 		raw_pvals_vector = robjects.FloatVector(raw_pvals)
 		padjust = robjects.r['p.adjust'](raw_pvals_vector, method=args.adj_method)
+		ColorText().info(" [done]\n", "stderr")
 
 		# output p-values
+		ColorText().info("[poolseq_tk]: output to files ...", "stderr")
 		out_all = args.outp + ".cmh.all"
 		out_fdr = args.outp + ".cmh.fdr%d" %(args.adj_cutoff*100)
-		out_expect = args.outp + ".cmh.fdr%d.expect" %(args.adj_cutoff*100)
-		with open(out_all, 'w') as fALL, open(out_fdr, 'w') as fFDR, open(out_expect, 'w') as fEXPECT:
+		sz_utils.make_dirs_if_necessary(out_all, out_fdr)
+		with open(out_all, 'w') as fALL, open(out_fdr, 'w') as fFDR:
 			for i, pos in enumerate(sorted(pvals.iterkeys())):
 				if padjust[i] <= args.adj_cutoff:
 					sz_utils._results_outputter(fFDR, pos, tables[pos][0], "\t".join(tables[pos][1:3]), tables[pos][3:], pvals[pos], padjust[i], odds_ratios[pos])
@@ -58,6 +84,8 @@ def run_cmh():
 #						(args.oddsr_direction == "less" and odds_ratios[pos] < 1)):
 #						sz_utils._results_outputter(fEXPECT, pos, tables[pos][0], "\t".join(tables[pos][1:3]), tables[pos][3:], pvals[pos], padjust[i], odds_ratios[pos])
 				sz_utils._results_outputter(fALL, pos, tables[pos][0], "\t".join(tables[pos][1:3]), tables[pos][3:], pvals[pos], padjust[i], odds_ratios[pos])
+		ColorText().info(" [done]\n", "stderr")
+		ColorText().info("[poolseq_tk]: Program finishes successfully\n", "stderr")
 
 def _cmh_worker(task_q, result_q, ntables_per_snp):
 	while True:
@@ -101,6 +129,6 @@ def _create_procs(nproc, task_q, result_q, ntables_per_snp):
 	''' initialize processes '''
 	ColorText().info("[poolseq_tk] Initializing processes ...\n", "stderr")
 	for _ in range(nproc):
-		p = mp.Process(target=cmh_worker, args=(task_q, result_q, ntables_per_snp))
+		p = mp.Process(target=_cmh_worker, args=(task_q, result_q, ntables_per_snp))
 		p.daemon = True
 		p.start()
