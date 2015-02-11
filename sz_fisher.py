cd12c87fa28b6af89b44219d0f1fa85e0fc9a7d0
Simo Zhang <zhangsimo850114@gmail.com>
2015-02-11 15:53:37 -0500

    morve out __bh_pvals_cutoff to sz_utils as getFDR_BH since it is shared; now output a [outp].expect with sites only having odds ratio in the right direction

--- sz_fisher.py
+++ sz_fisher.py
@@ -65,39 +65,26 @@ def run_fisher(args):
 		padjust = robjects.r['p.adjust'](raw_pvals_vector, method=args.adj_method)
 		ColorText().info(" [done]\n", "stderr")
 		ColorText().info("[poolseq_tk]: p-value cutoff using Benjamini.Hochberg procedure %.5e"
-						 %(__bh_pvals_cutoff(pvals, args.adj_cutoff)), "stderr")
+						 %(sz_utils.getFDR_BH(pvals, args.adj_cutoff)), "stderr")
 		ColorText().info(" [done]\n", "stderr")
 
 		# output p-values
 		ColorText().info("[poolseq_tk]: output to files ...", "stderr")
 		out_all = args.outp + ".fisher.all"
 		out_fdr = args.outp + ".fisher.fdr%d" %(args.adj_cutoff*100)
-#		out_expect = args.outp + ".fisher.fdr%d.expect" %(args.adj_cutoff*100)
-		with open(out_all, 'w') as fALL, open(out_fdr, 'w') as fFDR:
+		out_expect = args.outp + ".fisher.fdr%d.expect" %(args.adj_cutoff*100)
+		with open(out_all, 'w') as fALL, \
+			 open(out_fdr, 'w') as fFDR, \
+			 open(out_expect, 'w') as fEXPECT:
 			for i, pos in enumerate(sorted(pvals.iterkeys())):
 				if padjust[i] <= args.adj_cutoff:
 					sz_utils._results_outputter(fFDR, pos, tables[pos][0], "\t".join(tables[pos][1:3]), tables[pos][3:], pvals[pos], padjust[i], odds_ratios[pos])
-#					if ((args.oddsr_direction == "less" and odds_ratios[pos] < 1) or
-#						 args.oddsr_direction == "greater" and odds_ratios[pos] > 1):
-#						sz_utils._results_outputter(fEXPECT, pos, tables[pos][0], "\t".join(tables[pos][1:3]), tables[pos][3:], pvals[pos], padjust[i], odds_ratios[pos])
+					if ((args.oddsr_direction == "less" and odds_ratios[pos] < 1) or
+						 args.oddsr_direction == "greater" and odds_ratios[pos] > 1):
+						sz_utils._results_outputter(fEXPECT, pos, tables[pos][0], "\t".join(tables[pos][1:3]), tables[pos][3:], pvals[pos], padjust[i], odds_ratios[pos])
 				sz_utils._results_outputter(fALL, pos, tables[pos][0], "\t".join(tables[pos][1:3]), tables[pos][3:], pvals[pos], padjust[i], odds_ratios[pos])
 		ColorText().info(" [done]\n", "stderr")
 		ColorText().info("[poolseq_tk]: Program finishes successfully\n", "stderr")
-
-def __bh_pvals_cutoff(dPvals, fdr):
-	'''
-		Using BH procedure to calculate pvalue
-		cutoff at a FDR level
-	'''
-	lPvals = [dPvals[k] for k in dPvals.iterkeys()]
-	ntests = len(lPvals)
-	sort_lPvals = sorted(lPvals)
-	for i in range(len(sort_lPvals)):
-		if sort_lPvals[i] > (float(i+1)/ntests)*fdr :
-			return sort_lPvals[i - 1]
-	if i == len(sort_lPvals):
-		ColorText().error("[poolseq_tk] Fail to calculate pvalue cutoff\n")
-		sys.exit()
 
 def _create_procs(nproc, task_q, result_q):
 	''' initialize processes '''
