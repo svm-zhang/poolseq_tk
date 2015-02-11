991c697eb1fcc212c53358045d60fa4d17f6fbeb
Simo Zhang <zhangsimo850114@gmail.com>
2015-02-10 21:21:49 -0500

    now add fst annotation in INFO field by giving a file with FST values

--- sz_prepVCF.py
+++ sz_prepVCF.py
@@ -29,21 +29,48 @@ def getFilters(lfilters):
 					break
 	return dfilters
 
+def getFst(ifst, dfst):
+	with open(ifst, 'r') as fFST:
+		for line in fFST:
+			tmp_line = line.strip().split("\t")
+			chr = tmp_line[0]
+			pos = int(tmp_line[1])
+			fst = tmp_line[2]
+			if chr not in dfst:
+				dfst[chr] = [(pos, fst)]
+			else:
+				dfst[chr].append((pos, fst))
+
+	for k, v in dfst.items():
+		v.sort()
+	return dfst
+
 def run_prepVCF(args):
 	sz_utils.check_if_files_exist(args.infile)
+
+	dfst = collections.defaultdict(list)
+	if args.ifst:
+		dfst = getFst(args.ifst, dfst)
 
 	dfilters = getFilters(args.filters)
 	outVCFHeaders(args.samples, args.out)
 	with open(args.infile, "r") as fIN:
 		for line in fIN:
 			tmp_line = line.strip().split("\t")
-			chr = tmp_line[0]
-			pos = tmp_line[1]
+			chr = "2L"
+			pos = int(tmp_line[1])
 			refBase = tmp_line[2]
 			altBase = tmp_line[3]
 			pval = float(tmp_line[-3])
 			corrPval = float(tmp_line[-2])
 			ratio = float(tmp_line[-1])
+			fst = -1.0
+			if chr in dfst:
+				for j in range(len(dfst[chr])):
+					if pos == dfst[chr][j][0]:
+						fst = float(dfst[chr][j][1])
+						dfst[chr].pop(j)
+						break
 			if "ratio" in dfilters:
 				if ((dfilters["ratio"][0] == '<' and ratio >= dfilters["ratio"][1]) or
 					 dfilters["ratio"][0] == '>' and ratio <= dfilters["ratio"][1]):
@@ -52,10 +79,14 @@ def run_prepVCF(args):
 				pass
 			if "corrPval" in dfilters:		# fix later
 				pass
-			args.out.write("%s\t%s\t.\t%s\t%s\t.\t.\t"
-							%(chr, pos, refBase, altBase))
-			args.out.write("pval=%.5f;corrPval=%.5f;ratio=%.5f\t"
-							%(pval, corrPval, ratio))
+			args.out.write("2L\t%s\t.\t%s\t%s\t.\t.\t"
+							%(pos, refBase, altBase))
+			if fst == -1.0:
+				args.out.write("pval=%.5g;corrPval=%.5f;ratio=%.5f;fst=N/A\t"
+								%(pval, corrPval, ratio))
+			else:
+				args.out.write("pval=%.5g;corrPval=%.5f;ratio=%.5f;fst=%.5f\t"
+								%(pval, corrPval, ratio, fst))
 			args.out.write("GT:Table")
 			poolIndex = 1
 			for i in range(len(tmp_line[4:-3])):
