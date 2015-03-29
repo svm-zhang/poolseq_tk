3690d5555a311d49807e5f225f52ef0aeb1b4c5f
Simo Zhang <zhangsimo850114@gmail.com>
2015-03-28 22:29:43 -0400

    add a different parseReadsBases function which is more accurate

--- sz_utils.py
+++ sz_utils.py
@@ -111,7 +111,37 @@ def _results_outputter(fHANDLE, pos, chr, bases, tables, pval, log10_pval, corr_
 	fHANDLE.write("\t%.8f\t%s\t%.8f\t%.8f\n" %(pval, str(log10_pval), corr_pval, odds_ratio))
 	fHANDLE.flush()
 
-def parseReadsBases(reads_bases, refBase, altBase):
+def parseReadsBases(ref_base, alt_base, reads_bases):
+	''' collapse reads bases of the two pileup files '''
+	reads_bases_parsed = ""
+	if reads_bases != "":
+		i = 0
+		while i <= len(reads_bases)-1:
+			if reads_bases[i] == '.':
+				reads_bases_parsed += ref_base
+				i += 1
+			elif reads_bases[i] == ',':
+				reads_bases_parsed += ref_base.lower()
+				i += 1
+			elif reads_bases[i] == alt_base:
+				reads_bases_parsed += alt_base
+				i += 1
+			elif reads_bases[i] ==  alt_base.lower():
+				reads_bases_parsed += alt_base.lower()
+				i += 1
+			elif reads_bases[i] in ['+', '-']:
+				len_indel = int(re.search(r'\d+', reads_bases[i+1:i+3]).group())
+				i += len_indel + len(str(len_indel)) + 1
+			elif reads_bases[i] in ['N', 'n', '$', '*']:
+				i += 1
+			elif reads_bases[i] == '^':
+				i += 2
+			else:
+				reads_bases_parsed += reads_bases[i]
+				i += 1
+	return reads_bases_parsed
+
+def old_parseReadsBases(reads_bases, refBase, altBase):
 	'''
 		parsing the 5th column in a mpileup file
 		5th column represents the bases on the reads
@@ -122,26 +152,26 @@ def parseReadsBases(reads_bases, refBase, altBase):
 	dMultiBases = {}
 	nRefBases, nAltBases = 0, 0
 	nReadsBases = 0
-	reads_bases_viewed = ""
+	reads_bases_transformed = ""
 	while i < len(reads_bases):
 		# ref forward
 		if reads_bases[i] == '.':
-			reads_bases_viewed += refBase
+			reads_bases_transformed += refBase
 			nRefBases += 1
 			i += 1
 		# ref reverse
 		elif reads_bases[i] == ',':
-			reads_bases_viewed += refBase.lower()
+			reads_bases_transformed += refBase.lower()
 			nRefBases += 1
 			i += 1
 		# alt forward
 		elif reads_bases[i] == altBase:
-			reads_bases_viewed += altBase
+			reads_bases_transformed += altBase
 			nAltBases += 1
 			i += 1
 		# alt reverse
 		elif reads_bases[i] ==  altBase.lower():
-			reads_bases_viewed += altBase.lower()
+			reads_bases_transformed += altBase.lower()
 			nAltBases += 1
 			i += 1
 		# indels
@@ -151,9 +181,9 @@ def parseReadsBases(reads_bases, refBase, altBase):
 				i += 1
 				indel = '*'
 			else:
-				len_indel = int(reads_bases[i+1])
+				len_indel = int(re.search(r'\d+', reads_bases[i+1:i+3]).group())
+				i += len_indel + len(str(len_indel)) + 1
 				indel = reads_bases[i+2:i+2+len_indel].upper()
-				i += len_indel + 1 + 1
 			if indel not in dIndels:
 				dIndels[indel] = 1
 			else:
@@ -170,30 +200,7 @@ def parseReadsBases(reads_bases, refBase, altBase):
 				dMultiBases[reads_bases[i]] = 1
 			else:
 				dMultiBases[reads_bases[i]] += 1
-			reads_bases_viewed += reads_bases[i]
+			reads_bases_transformed += reads_bases[i]
 			i += 1
 		nReadsBases += 1
-	return reads_bases_viewed, nReadsBases, nRefBases, dMultiBases, dIndels
-
-def getSNPs(isnp):
-	'''
-		getting polymorphic sties from file
-		file format:
-					1. chr name
-					2. pos
-					3. ref allele
-					4. alt allele
-	'''
-	check_if_files_exist(isnp)
-	dSNPs = collections.defaultdict(tuple)
-	with open(isnp, 'r') as fSNPS:
-		for line in fSNPS:
-			tmp_line = line.strip().split("\t")
-			chr = tmp_line[0]
-			pos = int(tmp_line[1])
-			refBase = tmp_line[2]
-			altBase = tmp_line[3]
-			if not (chr, pos) in dSNPs:
-				dSNPs[chr, pos] = (refBase, altBase)
-
-	return dSNPs
+	return reads_bases_transformed, nReadsBases, nRefBases, dMultiBases, dIndels
