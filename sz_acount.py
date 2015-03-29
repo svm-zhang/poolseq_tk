ba9080c627c1752ac087c2720e48ac225f3f011a
Simo Zhang <zhangsimo850114@gmail.com>
2015-03-28 21:51:14 -0400

    rewrite run_count function, now you can specify a set of SNPs at which counting will happen, positions with zero coverage will be kept in the results, and these site should be eliminated before test because of low coverage

--- sz_acount.py
+++ sz_acount.py
@@ -24,7 +24,23 @@ from colortext import ColorText
 
 def run_count(args):
 	''' Counting alleles at each SNP in the given pileup files '''
+
+	dPos = {}
+	if args.pos:
+		ColorText().info("[poolseq_tk] reading SNPs positions:", "stderr")
+		with open(args.pos, 'r') as fPOS:
+			for line in fPOS:
+				tmp_line = line.strip().split("\t")
+				chr = tmp_line[0]
+				pos = int(tmp_line[1])
+				if (chr, pos) not in dPos:
+					dPos[chr, pos] = 1
+		ColorText().info(" %d\n" %(len(dPos)), "stderr")
+	else:
+		ColorText().info("[poolseq_tk] no SNP positions provided ... [skipped]\n", "stderr")
+
 	ac = collections.defaultdict(tuple)
+	fOUT = open(args.out, 'w')
 	for pileup in args.pileups:
 		sz_utils.check_if_files_exist(pileup)
 		nsnps = 0
@@ -35,22 +51,19 @@ def run_count(args):
 				tmp_line = line.strip().split("\t")
 				chr = tmp_line[0]
 				pos = int(tmp_line[1])
-				ref_base = tmp_line[2]
-				alt_base = tmp_line[3]
-				ref_count, alt_count = 0, 0
-				if tmp_line[-1] != "N/A":
-					ref_count = tmp_line[-1].count(ref_base) + \
-								tmp_line[-1].count(ref_base.lower())
-					alt_count = tmp_line[-1].count(alt_base) + \
-								tmp_line[-1].count(alt_base.lower())
-				if (chr, pos) not in ac:
-					ac[chr, pos] = [ref_base, alt_base, str(ref_count), str(alt_count)]
-				else:
-					ac[chr, pos] += [str(ref_count), str(alt_count)]
-#				if pos not in ac:
-#					ac[pos] = [chr, ref_base, alt_base, str(ref_count), str(alt_count)]
-#				else:
-#					ac[pos] += [str(ref_count), str(alt_count)]
+				if (chr, pos) in dPos:
+					ref_base = tmp_line[2]
+					alt_base = tmp_line[3]
+					nRefAlleles, nAltAlleles = 0, 0
+					if len(tmp_line) == 5:
+						nRefAlleles = tmp_line[-1].count(ref_base) + \
+									  tmp_line[-1].count(ref_base.lower())
+						nAltAlleles = tmp_line[-1].count(alt_base) + \
+									  tmp_line[-1].count(alt_base.lower())
+					if (chr, pos) not in ac:
+						ac[chr, pos] = [ref_base, alt_base, str(nRefAlleles), str(nAltAlleles)]
+					else:
+						ac[chr, pos] += [str(nRefAlleles), str(nAltAlleles)]
 		ColorText().info(" %d SNPs parsed\n" %(nsnps), "stderr")
 
 	fOUT = None
@@ -60,16 +73,92 @@ def run_count(args):
 		sz_utils.make_dirs_if_necessary(args.out)
 		fOUT = open(args.out, 'w')
 	ColorText().info("[poolseq_tk] outputting allele counts to table ...", "stderr")
-	for (chr, pos) in sorted(ac.iterkeys()):
+	for k in sorted(ac.iterkeys()):
+		chr = k[0]
+		pos = k[1]
 		i = 2
-		if len(ac[chr, pos][i:]) == 2*len(args.pileups):
-			fOUT.write("%s\t%d\t%s" %(chr, pos, "\t".join(ac[chr, pos][1:3])))
-			while i <= len(ac[chr, pos])-4:
-				fOUT.write("\t%s" %(":".join(ac[chr, pos][i:i+4])))
+		if len(ac[k][i:]) == 2*len(args.pileups):
+			fOUT.write("%s\t%d\t%s" %(chr, pos, "\t".join(ac[k][0:2])))
+			while i <= len(ac[k])-4:
+				fOUT.write("\t%s" %(":".join(ac[k][i:i+4])))
 				i += 4
 			fOUT.write("\n")
+	ColorText().info(" [done]\n", "stderr")
+	fOUT.close()
+
+def parseReadsBases(reads_bases, refBase, altBase):
+	i = 0
+	nRefAlleles, nAltAlleles = 0, 0
+	nOtherAlleles = 0
+	cov = 0
+	while i < len(reads_bases):
+		if reads_bases[i] == '.':
+			nRefAlleles += 1
+			i += 1
+		elif reads_bases[i] == ',':
+			nRefAlleles += 1
+			i += 1
+		elif reads_bases[i] == altBase:
+			nAltAlleles += 1
+			i += 1
+		elif reads_bases[i] ==  altBase.lower():
+			nAltAlleles += 1
+			i += 1
+		elif reads_bases[i] in ['+', '-', '*']:
+			if reads_bases[i] == '*':
+				i += 1
+			else:
+				len_indel = int(re.search(r'\d+', reads_bases[i+1:i+3]).group())
+				i += len_indel + len(str(len_indel)) + 1
+		elif reads_bases[i] == '^':
+			i += 2
+		elif reads_bases[i] in ['N', 'n', '$']:
+			i += 1
 		else:
-			pass
+			nOtherAlleles += 1
+			i += 1
+		cov += 1
+	return cov, nRefAlleles, nAltAlleles, nOtherAlleles
+
+#def run_count(args):
+#	''' Counting alleles at each SNP in the given pileup files '''
+#	ac = collections.defaultdict(tuple)
+#	fOUT = open(args.out, 'w')
+#	for pileup in args.pileups:
+#		sz_utils.check_if_files_exist(pileup)
+#		nsnps = 0
+#		ColorText().info("[poolseq_tk] counting alleles in %s:" %(os.path.basename(pileup)), "stderr")
+#		with open(pileup, 'r') as fMPILEUP:
+#			for line in fMPILEUP:
+#				nsnps += 1
+#				tmp_line = line.strip().split("\t")
+#				chr = tmp_line[0]
+#				pos = int(tmp_line[1])
+#				ref_base = tmp_line[2]
+#				alt_base = tmp_line[3]
+#				ref_count, alt_count = 0, 0
+#				baseCounts = {'A': 0, 'C': 0, 'G': 0, 'T': 0, 'N': 0}
+#				if tmp_line[-1] != "N/A":
+#					ref_count = tmp_line[-1].count(ref_base) + \
+#								tmp_line[-1].count(ref_base.lower())
+#					alt_count = tmp_line[-1].count(alt_base) + \
+#								tmp_line[-1].count(alt_base.lower())
+#				if (chr, pos) not in ac:
+#					ac[chr, pos] = [ref_base, alt_base, str(ref_count), str(alt_count)]
+#				else:
+#					ac[chr, pos] += [str(ref_count), str(alt_count)]
+#				for k in baseCounts.iterkeys():
+#					baseCounts[k] = tmp_line[-1].count(k) + \
+#									tmp_line[-1].count(k.lower())
+#				fOUT.write("%s\t%s\t%d:%d:%d:%d:%d\n" %(chr, pos, baseCounts['A'], baseCounts['T'], baseCounts['C'], baseCounts['G'], baseCounts['N']))
+#				if pos not in ac:
+#					ac[pos] = [chr, ref_base, alt_base, str(ref_count), str(alt_count)]
+#				else:
+#					ac[pos] += [str(ref_count), str(alt_count)]
+#		ColorText().info(" %d SNPs parsed\n" %(nsnps), "stderr")
+#	ColorText().info(" [done]\n", "stderr")
+#	fOUT.close()
+
 #		if len(args.pileups) != 1:
 #			# this deals with case where some pools have no data at this site
 #			if len(ac[pos][i:]) >= 2*len(args.pileups):
@@ -84,5 +173,3 @@ def run_count(args):
 #				fOUT.write("\t%s" %(":".join(ac[pos][i:i+2])))
 #				i += 2
 #			fOUT.write("\n")
-	ColorText().info(" [done]\n", "stderr")
-	fOUT.close()
